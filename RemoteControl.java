74d112fe9117520b0be371311bf0ba23b2ca61c3
ChoYeonJu <yjjoh@gmail.com>
2019-04-15 18:37:27 +0900

    Add RemoteControl.java

---
+++ RemoteControl.java
@@ -0,0 +1,23 @@
+package interfaceEx1;
+
+public interface RemoteControl {
+	public int MAX_VOLUME = 10;
+	public int MIN_VOLUME = 0;
+	
+	public void turnOn();
+	public void turnOff();
+	public void setVolume(int volume);
+	
+	default void setMute(boolean mute) {		//public 의 특성을 갖음
+		if(mute) {
+			System.out.println("무음 처리합니다.");
+		}else {
+			System.out.println("무음 해제합니다.");
+		}
+	}
+	
+	static void changeBattery() {
+		System.out.println("건전지를 교환합니다.");
+	}
+
+}
